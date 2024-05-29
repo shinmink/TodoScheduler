cd8b9b542dfd29ca092e210986cfa0b4552ee1d7
shinmink <sinmk99@hansung.ac.kr>
2024-05-29 21:14:38 +0900

    second init commit

---
+++ src/main/java/com/sparta/todoscheduler/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.todoscheduler.repository;
+
+import com.sparta.todoscheduler.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/sparta/todoscheduler/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sparta.todoscheduler.security;
+
+import com.sparta.todoscheduler.entity.User;
+import com.sparta.todoscheduler.repository.UserRepository;
+import com.sparta.todoscheduler.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
